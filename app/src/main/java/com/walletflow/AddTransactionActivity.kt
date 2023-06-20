27d6978050689ef455155e57920e5c274c8d7652
jayhaytch <francesco.zonaro@gmail.com>
2023-06-20 12:48:18 +0200

    transactions page

---
+++ app/src/main/java/com/walletflow/AddTransactionActivity.kt
@@ -0,0 +1,15 @@
+package com.walletflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class AddTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_transaction)
+
+
+    }
+}
--- app/src/main/java/com/walletflow/HomeActivity.kt
+++ app/src/main/java/com/walletflow/HomeActivity.kt
@@ -1,11 +1,37 @@
 package com.walletflow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var earningBtn : Button
+    lateinit var expenseBtn : Button
+
+    companion object {
+        const val EARNING_CONST = 1
+        const val EXPENSE_CONST = -1
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        earningBtn = findViewById(R.id.btnAddEarning)
+        expenseBtn = findViewById(R.id.btnAddExpenses)
+
+        earningBtn.setOnClickListener {
+            val intent = Intent(this, AddTransactionActivity::class.java)
+            intent.putExtra("type", HomeActivity.EARNING_CONST)
+            startActivity(intent)
+        }
+
+        expenseBtn.setOnClickListener {
+            val intent = Intent(this, AddTransactionActivity::class.java)
+            intent.putExtra("type", HomeActivity.EXPENSE_CONST)
+            startActivity(intent)
+        }
     }
 }
