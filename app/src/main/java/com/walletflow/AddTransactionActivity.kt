656b93a6d23d29570ee85ec9d2503b24a3721cd1
jayhaytch <francesco.zonaro@gmail.com>
2023-06-21 12:54:45 +0200

    adding the transaction in the db

--- app/src/main/java/com/walletflow/AddTransactionActivity.kt
+++ app/src/main/java/com/walletflow/AddTransactionActivity.kt
@@ -1,11 +1,17 @@
 package com.walletflow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AddTransactionActivity : AppCompatActivity() {
 
@@ -16,6 +22,7 @@ class AddTransactionActivity : AppCompatActivity() {
     lateinit var noteEditText : EditText
     lateinit var recurrentCheck : CheckBox
     lateinit var chooseCategoryBtn : Button
+    lateinit var selectorRecurrent : Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +38,66 @@ class AddTransactionActivity : AppCompatActivity() {
         noteEditText = findViewById(R.id.etNote)
         recurrentCheck = findViewById(R.id.cbRecurrent)
         chooseCategoryBtn = findViewById(R.id.btnChooseCategory)
+        selectorRecurrent = findViewById(R.id.spRecurrent)
 
         titleTv.text = typeName?.uppercase()
         subtitleTv.append(typeName)
         amountTitleTv.append(typeVerb + "?")
 
+        recurrentCheck.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                selectorRecurrent.visibility = View.VISIBLE
+            } else {
+                selectorRecurrent.visibility = View.GONE
+            }
+        }
+
+        chooseCategoryBtn.setOnClickListener {
+
+            val amount = amountEditText.text.toString()
+            val note = noteEditText.text.toString()
+            val recurrent = recurrentCheck.isChecked
+            var recurrency = "None"
+
+            if(recurrent){
+                recurrency = selectorRecurrent.selectedItem.toString()
+            }
+
+            if(amount.isEmpty()){
+                Toast.makeText(this, "Please specify the amount", Toast.LENGTH_LONG).show()
+            }
+            else {
+                val db = FirebaseFirestore.getInstance()
+                addTransaction(db, amount.toFloat(), note, recurrency)
+            }
+
+        }
+
+    }
+
+    private fun addTransaction(db : FirebaseFirestore, amount : Float, note : String, recurrency : String){
+        val transaction: MutableMap<String, Any> = HashMap()
+        transaction["amount"] = amount
+        transaction["note"] = note
+        transaction["recurrency"] = recurrency
+
+        db.collection("transactions")
+            .add(transaction)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    this.localClassName,
+                    "DocumentSnapshot added with ID: " + documentReference.id
+                )
+            }
+            .addOnFailureListener { e ->
+                Log.w(
+                    this.localClassName,
+                    "Error adding document",
+                    e
+                )
+            }
+
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
