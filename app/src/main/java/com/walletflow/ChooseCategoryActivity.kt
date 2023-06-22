8320a9649a9c26016f31f8c599ef14477733b488
jayhaytch <francesco.zonaro@gmail.com>
2023-06-22 15:34:32 +0200

    category activity

---
+++ app/src/main/java/com/walletflow/ChooseCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.walletflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_category)
+    }
+}
+
