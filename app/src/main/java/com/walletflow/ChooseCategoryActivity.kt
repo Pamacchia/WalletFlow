e47dbb813a9bcebe01d72f21d4f7239faa17b1d9
jayhaytch <francesco.zonaro@gmail.com>
2023-06-23 12:06:25 +0200

    icons and category page

--- app/src/main/java/com/walletflow/ChooseCategoryActivity.kt
+++ app/src/main/java/com/walletflow/ChooseCategoryActivity.kt
@@ -7,6 +7,7 @@ class ChooseCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_category)
+
+        
     }
 }
-
