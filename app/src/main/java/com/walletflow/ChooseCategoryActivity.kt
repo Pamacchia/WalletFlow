d7e2bf43e2553bc598c3fa46937aeda074220fb1
Pamacchia <80270757+Pamacchia@users.noreply.github.com>
2023-06-24 13:09:16 +0200

    ci siamo quasi (speriamo)

--- app/src/main/java/com/walletflow/ChooseCategoryActivity.kt
+++ app/src/main/java/com/walletflow/ChooseCategoryActivity.kt
@@ -1,33 +1,69 @@
 package com.walletflow
 
 import android.content.Intent
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
+import java.io.IOException
+
 
 class ChooseCategoryActivity : AppCompatActivity() {
 
-    lateinit var submitBtn : Button
+    //lateinit var submitBtn : Button
     lateinit var selected : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_category)
 
-        submitBtn = findViewById(R.id.btnSubmitCategory)
+        //submitBtn = findViewById(R.id.btnSubmitCategory)
 
-        val resources = resources
+        val assetManager = resources.assets
+        val fileList: Array<String>? = assetManager.list("icons")
 
-        submitBtn.setOnClickListener {
+        loadIcons(fileList)
+
+        /*submitBtn.setOnClickListener {
             val db = FirebaseFirestore.getInstance()
             addTransaction(db, intent.getFloatExtra("amount", 0F),
                 intent.getStringExtra("note"), intent.getStringExtra("recurrency"),
                 intent.getStringExtra("userID"), intent.getStringExtra("typeName"), selected)
+        }*/
+    }
+
+    //TODO: da finire non va
+    private fun loadIcons(fileList : Array<String>?){
+
+        val rootView = findViewById<ViewGroup>(android.R.id.content)
+
+        for (fileName in fileList!!) {
+            val imageView = ImageView(this)
+
+            try {
+                // Open the input stream for the image file in assets
+                val inputStream = assets.open("icons/my_image.png")
+
+                // Create a Drawable from the input stream
+                val drawable = Drawable.createFromStream(inputStream, null)
+
+                // Set the drawable as the image source for the ImageView
+                imageView.setImageDrawable(drawable)
+
+                // Close the input stream
+                inputStream.close()
+                rootView.addView(imageView)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+            imageView.layoutParams = layoutParams
         }
     }
 
