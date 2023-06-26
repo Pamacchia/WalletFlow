8ea354b8e0c6413e6616167790cd4691498d73ba
jayhaytch <francesco.zonaro@gmail.com>
2023-06-26 13:14:58 +0200

    category selection and db transaction submit

--- app/src/main/java/com/walletflow/ChooseCategoryActivity.kt
+++ app/src/main/java/com/walletflow/ChooseCategoryActivity.kt
@@ -7,40 +7,40 @@ import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.api.Distribution.BucketOptions.Linear
 import com.google.firebase.firestore.FirebaseFirestore
 import java.io.IOException
 
 
 class ChooseCategoryActivity : AppCompatActivity() {
 
-    //lateinit var submitBtn : Button
+    lateinit var submitBtn : Button
     lateinit var selected : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_category)
 
-        //submitBtn = findViewById(R.id.btnSubmitCategory)
-
         val assetManager = resources.assets
         val fileList: Array<String>? = assetManager.list("icons/test")
-
         loadIcons(fileList)
 
-        /*submitBtn.setOnClickListener {
+        submitBtn = findViewById(R.id.btnSubmitCategory)
+
+        submitBtn.setOnClickListener {
             val db = FirebaseFirestore.getInstance()
             addTransaction(db, intent.getFloatExtra("amount", 0F),
                 intent.getStringExtra("note"), intent.getStringExtra("recurrency"),
                 intent.getStringExtra("userID"), intent.getStringExtra("typeName"), selected)
-        }*/
+        }
     }
 
-    //TODO: da finire non va
     private fun loadIcons(fileList : Array<String>?){
 
-        val rootView = findViewById<LinearLayout>(R.id.mainLinearLayout)
+        val rootView = findViewById<LinearLayout>(R.id.iconsLinearLayout)
 
         var count = 0
         lateinit var linearLayout : LinearLayout
@@ -73,9 +73,13 @@ class ChooseCategoryActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
 
-            imageView.layoutParams.height=LayoutParams.WRAP_CONTENT
-            imageView.layoutParams.width=LayoutParams.WRAP_CONTENT
+            val factor: Float = this.resources.displayMetrics.density
+
+            imageView.layoutParams = LinearLayout.LayoutParams((factor*100).toInt(), (factor*100).toInt(), 1F)
             imageView.tag= fileName.split(".")[0]
+            imageView.setOnClickListener {
+                showSelected(imageView)
+            }
             count++
         }
     }
