e18e2f60ae87a4217e8aacb0a7933bf83df19fdb
jayhaytch <francesco.zonaro@gmail.com>
2023-07-05 11:17:57 +0200

    comments

--- app/src/main/java/com/walletflow/utils/SQLiteDBHelper.kt
+++ app/src/main/java/com/walletflow/utils/SQLiteDBHelper.kt
@@ -6,112 +6,49 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class SQLiteDBHelper(context: Context, factory : SQLiteDatabase.CursorFactory?)
-    : SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+class SQLiteDBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
 
-    companion object{
-        // below is variable for database name
+    companion object {
         private val DATABASE_NAME = "ICONS.sqlite"
-
-        // below is the variable for database version
         private val DATABASE_VERSION = 1
-
-        // below is the variable for table name
         val CATEGORY_TABLE = "category_table"
     }
 
-        // below is the method for creating a database by a sqlite query
-        override fun onCreate(db: SQLiteDatabase) {
-            // below is a sqlite query, where column names
-            // along with their data types is given
-            val query = ("CREATE TABLE " + CATEGORY_TABLE + " ("
-                    + "id" + " INTEGER PRIMARY KEY, " +
-                    "file_path" + " TEXT," +
-                    "added" + " INTEGER" + ");")
+    override fun onCreate(db: SQLiteDatabase) {
+        val query = ("CREATE TABLE " + CATEGORY_TABLE + " ("
+                + "id" + " INTEGER PRIMARY KEY, " +
+                "file_path" + " TEXT," +
+                "added" + " INTEGER" + ");")
 
-            // we are calling sqlite
-            // method for executing our query
-            db.execSQL(query)
+        db.execSQL(query)
 
-            val values = ContentValues()
+        val values = ContentValues()
 
-            val array1 = arrayOf("food.png", "popcorn.png", "technology.png", "tshirt.png", "transport.png")
-            val array2 = arrayOf(1, 1, 1, 0, 0)
+        val array1 =
+            arrayOf("food.png", "popcorn.png", "technology.png", "tshirt.png", "transport.png")
+        val array2 = arrayOf(1, 1, 1, 0, 0)
 
-            // Check if the arrays have the same size
-            for (i in array1.indices) {
-                values.put("file_path", array1[i])
-                values.put("added", array2[i])
+        for (i in array1.indices) {
+            values.put("file_path", array1[i])
+            values.put("added", array2[i])
 
-                db.insert(CATEGORY_TABLE, null, values)
-            }
-        }
-
-        override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
-            // this method is to check if table already exists
-            db.execSQL("DROP TABLE IF EXISTS " + CATEGORY_TABLE)
-            onCreate(db)
-        }
-
-        // This method is for adding data in our database
-        fun addName(path : String, added : String ){
-
-            // below we are creating
-            // a content values variable
-            val values = ContentValues()
-
-            // we are inserting our values
-            // in the form of key-value pair
-            values.put("file_path", path)
-            values.put("added", added)
-
-            // here we are creating a
-            // writable variable of
-            // our database as we want to
-            // insert value in our database
-            val db = this.writableDatabase
-
-            // all values are inserted into database
             db.insert(CATEGORY_TABLE, null, values)
-
-            // at last we are
-            // closing our database
-            db.close()
         }
+    }
 
-        // below method is to get
-        // all data from our database
-        fun getName(): Cursor? {
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + CATEGORY_TABLE)
+        onCreate(db)
+    }
 
-            // here we are creating a readable
-            // variable of our database
-            // as we want to read value from it
-            val db = this.readableDatabase
+    fun getAlreadyAdded(): Cursor? {
+        val db = this.readableDatabase
+        return db.rawQuery("SELECT file_path FROM " + CATEGORY_TABLE + " WHERE added = 1", null)
+    }
 
-            // below code returns a cursor to
-            // read data from the database
-            return db.rawQuery("SELECT * FROM " + CATEGORY_TABLE, null)
-
-        }
-
-        fun emptyTable() {
-            val db = this.writableDatabase
-            db.delete(CATEGORY_TABLE, null, null)
-        }
-
-        fun getAlreadyAdded(): Cursor? {
-            val db = this.readableDatabase
-
-            // below code returns a cursor to
-            // read data from the database
-            return db.rawQuery("SELECT file_path FROM " + CATEGORY_TABLE + " WHERE added = 1", null)
-        }
-
-        fun getToAdd(): Cursor? {
-            val db = this.readableDatabase
-
-            // below code returns a cursor to
-            // read data from the database
-            return db.rawQuery("SELECT file_path FROM " + CATEGORY_TABLE + " WHERE added = 0", null)
-        }
+    fun getToAdd(): Cursor? {
+        val db = this.readableDatabase
+        return db.rawQuery("SELECT file_path FROM " + CATEGORY_TABLE + " WHERE added = 0", null)
+    }
 }
