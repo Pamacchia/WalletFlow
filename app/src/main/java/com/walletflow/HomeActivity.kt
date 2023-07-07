b79946acafb139029675583f42470c7fc6aa8198
Pamacchia <80270757+Pamacchia@users.noreply.github.com>
2023-07-07 11:17:02 +0200

    home

--- app/src/main/java/com/walletflow/HomeActivity.kt
+++ app/src/main/java/com/walletflow/HomeActivity.kt
@@ -1,15 +1,19 @@
 package com.walletflow
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
 
+//TODO : show in home balance and objective's balance (perché non lo calcoliamo più in maniera dinamica e l'utente può recuoerare soldi)
 class HomeActivity : AppCompatActivity() {
 
     lateinit var earningBtn : Button
     lateinit var expenseBtn : Button
-
+    lateinit var balanceTv : TextView
     companion object {
         const val EARNING_CONST = 1
         const val EXPENSE_CONST = -1
@@ -25,6 +29,9 @@ class HomeActivity : AppCompatActivity() {
 
         earningBtn = findViewById(R.id.btnAddEarning)
         expenseBtn = findViewById(R.id.btnAddExpenses)
+        balanceTv = findViewById(R.id.tvBalance)
+
+        balanceTv.text = loadBalance().toString()
 
         earningBtn.setOnClickListener {
             val intent = Intent(this, AddTransactionActivity::class.java)
@@ -41,5 +48,24 @@ class HomeActivity : AppCompatActivity() {
             intent.putExtra("type_verb", SPEND)
             startActivity(intent)
         }
+    }
+
+    private fun loadBalance() : Float{
+
+        var balance : Float = 0f
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val userID = sharedPreferences.getString("userID", "")
+
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("users").whereEqualTo("username", userID).limit(1).get()
+            .addOnSuccessListener { document ->
+                balance = document.first().getString("balance")!!.toFloat()
+            }
+            .addOnFailureListener { e ->
+                println("Error getting documents: $e")
+            }
+
+        return balance
     }
 }
