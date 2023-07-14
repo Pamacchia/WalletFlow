d5157635a14c5ae9bece531e84682b6e803a96ce
francescozonaro <francesco.zonaro@gmail.com>
2023-07-14 13:11:29 +0200

    navigation bar

--- app/src/main/java/com/walletflow/BaseActivity.kt
+++ app/src/main/java/com/walletflow/BaseActivity.kt
@@ -1,21 +1,38 @@
 package com.walletflow
 
+import android.content.ClipData.Item
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.walletflow.objectives.ObjectivesActivity
 
 abstract class BaseActivity : AppCompatActivity() {
 
+    lateinit var itemOpenHome : View
+    lateinit var itemOpenObjectives : View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutResourceId())
+
+        itemOpenHome = findViewById(R.id.nav_home)
+        itemOpenObjectives = findViewById(R.id.nav_objectives)
+
+        itemOpenHome.setOnClickListener {
+            openHomeActivity()
+        }
+
+        itemOpenObjectives.setOnClickListener {
+            openObjectivesActivity()
+        }
+
     }
 
     abstract fun getLayoutResourceId(): Int
 
-    fun openHomeActivity(item: MenuItem) {
+    private fun openHomeActivity() {
         // Launch the HomeActivity
         if(!HomeActivity::class.java.name.contains(this.localClassName)){
             val intent = Intent(this, HomeActivity::class.java)
@@ -23,7 +40,7 @@ abstract class BaseActivity : AppCompatActivity() {
         }
     }
 
-    fun openObjectivesActivity(item: MenuItem) {
+    private fun openObjectivesActivity() {
         // Launch the HomeActivity
         if(!ObjectivesActivity::class.java.name.contains(this.localClassName)){
             val intent = Intent(this, ObjectivesActivity::class.java)
