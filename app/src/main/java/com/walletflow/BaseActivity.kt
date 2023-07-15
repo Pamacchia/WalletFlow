a1b4f76c45fa9fb5517a1de2453daed782c5c290
Pamacchia <80270757+Pamacchia@users.noreply.github.com>
2023-07-15 12:54:21 +0200

    nav_bar2

--- app/src/main/java/com/walletflow/BaseActivity.kt
+++ app/src/main/java/com/walletflow/BaseActivity.kt
@@ -1,31 +1,41 @@
 package com.walletflow
 
-import android.content.ClipData.Item
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.walletflow.objectives.ObjectivesActivity
 
 abstract class BaseActivity : AppCompatActivity() {
 
-    lateinit var itemOpenHome : View
-    lateinit var itemOpenObjectives : View
+    lateinit var bottomNavBar : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutResourceId())
+        bottomNavBar = findViewById(R.id.bottom_navigation)
+        bottomNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_settings -> {
 
-        itemOpenHome = findViewById(R.id.nav_home)
-        itemOpenObjectives = findViewById(R.id.nav_objectives)
+                    true
+                }
+                R.id.nav_dashboard -> {
 
-        itemOpenHome.setOnClickListener {
-            openHomeActivity()
-        }
+                    true
+                }
+                R.id.nav_objectives -> {
+                    openObjectivesActivity()
+                    true
+                }
 
-        itemOpenObjectives.setOnClickListener {
-            openObjectivesActivity()
+                else -> {
+                    openHomeActivity()
+                    true
+                }
+            }
         }
 
     }
@@ -33,7 +43,6 @@ abstract class BaseActivity : AppCompatActivity() {
     abstract fun getLayoutResourceId(): Int
 
     private fun openHomeActivity() {
-        // Launch the HomeActivity
         if(!HomeActivity::class.java.name.contains(this.localClassName)){
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
