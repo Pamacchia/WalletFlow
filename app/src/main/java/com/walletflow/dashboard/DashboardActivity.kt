73205f42ccefc95ee54100926a7c6149184d174e
francescozonaro <francesco.zonaro@gmail.com>
2023-07-18 11:36:52 +0200

    dashboard

--- app/src/main/java/com/walletflow/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/walletflow/dashboard/DashboardActivity.kt
@@ -7,6 +7,5 @@ import com.walletflow.R
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
     }
 }
