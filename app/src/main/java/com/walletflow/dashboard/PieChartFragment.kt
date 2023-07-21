fe0c211edcb844ce6a59c98251574d92146f3c20
Pamacchia <80270757+Pamacchia@users.noreply.github.com>
2023-07-21 12:35:37 +0200

    PieChart

--- app/src/main/java/com/walletflow/dashboard/PieChartFragment.kt
+++ app/src/main/java/com/walletflow/dashboard/PieChartFragment.kt
@@ -1,28 +1,71 @@
 package com.walletflow.dashboard
 
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 import com.walletflow.R
 
+
 class PieChartFragment : Fragment() {
+
+    lateinit var pieChart : PieChart;
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
 
-        val chart = getView()?.findViewById(R.id.pieChartCategories) as BarChart
-
-        val data = BarData(getXAxisValues(), getDataSet())
-        chart.data = data
-        chart.animateXY(2000, 2000)
-        chart.invalidate()
+        pieChart = getView()?.findViewById(R.id.pieChartCategories) as PieChart
+        showPieChart()
 
         return inflater.inflate(R.layout.fragment_pie_chart, container, false)
+    }
+
+    private fun showPieChart() {
+        val pieEntries = ArrayList<PieEntry>()
+        val label = "type"
+
+        //initializing data
+        val typeAmountMap: MutableMap<String, Int> = HashMap()
+        typeAmountMap["Toys"] = 200
+        typeAmountMap["Snacks"] = 230
+        typeAmountMap["Clothes"] = 100
+        typeAmountMap["Stationary"] = 500
+        typeAmountMap["Phone"] = 50
+
+        //initializing colors for the entries
+        val colors = ArrayList<Int>()
+        colors.add(Color.parseColor("#304567"))
+        colors.add(Color.parseColor("#309967"))
+        colors.add(Color.parseColor("#476567"))
+        colors.add(Color.parseColor("#890567"))
+        colors.add(Color.parseColor("#a35567"))
+        colors.add(Color.parseColor("#ff5f67"))
+        colors.add(Color.parseColor("#3ca567"))
+
+        //input data and fit data into pie chart entry
+        for (type in typeAmountMap.keys) {
+            pieEntries.add(PieEntry(typeAmountMap[type]!!.toFloat(), type))
+        }
+
+        //collecting the entries with label name
+        val pieDataSet = PieDataSet(pieEntries, label)
+        //setting text size of the value
+        pieDataSet.valueTextSize = 12f
+        //providing color list for coloring different entries
+        pieDataSet.colors = colors
+        //grouping the data set from entry to chart
+        val pieData = PieData(pieDataSet)
+        //showing the value of the entries, default true if not set
+        pieData.setDrawValues(true)
+        pieChart.setData(pieData)
+        pieChart.invalidate()
     }
 
 }
