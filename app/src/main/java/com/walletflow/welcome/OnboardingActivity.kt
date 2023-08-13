3af75885eb0480dfa2fc7ff595a98abb6f1ee44c
francescozonaro <francesco.zonaro@gmail.com>
2023-08-13 15:02:00 +0200

    OnboardingActivity format and warnings

--- app/src/main/java/com/walletflow/welcome/OnboardingActivity.kt
+++ app/src/main/java/com/walletflow/welcome/OnboardingActivity.kt
@@ -2,18 +2,18 @@ package com.walletflow.welcome
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import com.walletflow.HomeActivity
 import com.walletflow.R
 
 class OnboardingActivity : AppCompatActivity() {
 
-    lateinit var btnStart : Button
-    lateinit var etBalance : EditText
+    private lateinit var btnStart: Button
+    private lateinit var etBalance: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +40,11 @@ class OnboardingActivity : AppCompatActivity() {
         }
     }
 
-    fun modifyRecordByUsername(collectionName: String, username: String?, updatedFields: Map<String, Any>) {
+    private fun modifyRecordByUsername(
+        collectionName: String,
+        username: String?,
+        updatedFields: Map<String, Any>
+    ) {
 
         val db = FirebaseFirestore.getInstance()
 
