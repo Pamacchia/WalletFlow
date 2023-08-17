d4c9099623fa40c2cf67d4e38999d78bb3c7fff8
Pamacchia <80270757+Pamacchia@users.noreply.github.com>
2023-08-17 17:19:59 +0200

    result choosecategory

--- app/src/main/java/com/walletflow/transactions/AddTransactionActivity.kt
+++ app/src/main/java/com/walletflow/transactions/AddTransactionActivity.kt
@@ -1,20 +1,25 @@
 package com.walletflow.transactions
 
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import com.walletflow.BaseActivity
 import com.walletflow.R
+import com.walletflow.utils.TransactionManager
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class AddTransactionActivity : BaseActivity() {
 
-
+    private val chooseCategoryRequestCode = 1
     lateinit var amountEditText: EditText
     lateinit var noteEditText: EditText
     lateinit var frequentCheck: CheckBox
@@ -22,10 +27,6 @@ class AddTransactionActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val type = intent.getIntExtra("type", 0)
-        val typeName = intent.getStringExtra("type_name")
-        val typeVerb = intent.getStringExtra("type_verb")
 
         amountEditText = findViewById(R.id.etAmount)
         noteEditText = findViewById(R.id.etNote)
@@ -43,21 +44,106 @@ class AddTransactionActivity : BaseActivity() {
 
             if (amount.isEmpty()) {
                 Toast.makeText(this, "Please specify the amount", Toast.LENGTH_LONG).show()
-            } else if(noteEditText.text.isEmpty() && frequentCheck.isChecked) {
+            } else if(note.isEmpty() && frequent) {
                 Toast.makeText(this, "Please add a note for frequent transactions", Toast.LENGTH_LONG).show()
             } else {
                 val intent = Intent(this, ChooseCategoryActivity::class.java)
-                intent.putExtra("amount", amount.toFloat() * type)
-                intent.putExtra("note", note)
-                intent.putExtra("frequent", frequent)
-                intent.putExtra("userID", userID)
-                intent.putExtra("typeName", typeName)
-                startActivity(intent)
-                finish()
+                intent.putExtra("typeName", this.intent.getStringExtra("type_name"))
+                startActivityForResult(intent, chooseCategoryRequestCode)
             }
 
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+
+        val amount = amountEditText.text.toString()
+        val note = noteEditText.text.toString()
+        val frequent = frequentCheck.isChecked
+        val type = intent.getIntExtra("type", 0)
+        val typeName = intent.getStringExtra("type_name")
+
+        if (requestCode == chooseCategoryRequestCode && resultCode == Activity.RESULT_OK) {
+            intentData?.let { data ->
+
+                addTransaction(
+                    db, amount.toFloat() * type,
+                    note, frequent, userID, typeName, data.getStringExtra("category")
+                )
+
+                TransactionManager.updateBalance(
+                    db,
+                    amount.toFloat() * type,
+                    userID
+                )
+                Thread.sleep(150L)
+                finish()
+            }
+        }
+    }
+
+    private fun addTransaction(
+        db: FirebaseFirestore,
+        amount: Float,
+        note: String?,
+        frequent: Boolean,
+        userID: String?,
+        type_name: String?,
+        category: String?
+    ) {
+        val transaction: MutableMap<String, Any?> = HashMap()
+        transaction["user"] = userID
+        transaction["type"] = type_name
+        transaction["amount"] = amount
+        transaction["note"] = note
+        transaction["category"] = category
+        transaction["date"] =
+            SimpleDateFormat("yyyy-MM-dd HH:mm").format(Calendar.getInstance().time)
+
+        db.collection("transactions")
+            .add(transaction)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    this.localClassName,
+                    "DocumentSnapshot added with ID: " + documentReference.id
+                )
+            }
+            .addOnFailureListener { e ->
+                Log.w(
+                    this.localClassName,
+                    "Error adding document",
+                    e
+                )
+            }
+
+
+        if (frequent) {
+
+            val frequentTransaction: MutableMap<String, Any?> = HashMap()
+            frequentTransaction["user"] = userID
+            frequentTransaction["type"] = type_name
+            frequentTransaction["amount"] = amount
+            frequentTransaction["note"] = note
+            frequentTransaction["category"] = category
+
+            db.collection("frequentTransactions")
+                .add(frequentTransaction)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(
+                        this.localClassName,
+                        "DocumentSnapshot added with ID: " + documentReference.id
+                    )
+                }
+                .addOnFailureListener { e ->
+                    Log.w(
+                        this.localClassName,
+                        "Error adding document",
+                        e
+                    )
+                }
+        }
     }
 
     override fun getLayoutResourceId(): Int {
--- app/src/main/java/com/walletflow/transactions/ChooseCategoryActivity.kt
+++ app/src/main/java/com/walletflow/transactions/ChooseCategoryActivity.kt
@@ -1,5 +1,6 @@
 package com.walletflow.transactions
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -30,21 +31,7 @@ class ChooseCategoryActivity : CategoryActivity() {
         loadIcons(iconList)
 
         submitBtn.setOnClickListener {
-            val db = FirebaseFirestore.getInstance()
-
-            addTransaction(
-                db, intent.getFloatExtra("amount", 0F),
-                intent.getStringExtra("note"), intent.getBooleanExtra("frequent", false),
-                intent.getStringExtra("userID"), intent.getStringExtra("typeName"), selected
-            )
-
-            TransactionManager.updateBalance(
-                db,
-                intent.getFloatExtra("amount", 0F),
-                intent.getStringExtra("userID")
-            )
-            Thread.sleep(150L)
-            finish()
+            selectCategory()
         }
     }
 
@@ -58,66 +45,11 @@ class ChooseCategoryActivity : CategoryActivity() {
         loadIcons(iconList)
     }
 
-    private fun addTransaction(
-        db: FirebaseFirestore,
-        amount: Float,
-        note: String?,
-        frequent: Boolean,
-        userID: String?,
-        type_name: String?,
-        category: String?
-    ) {
-        val transaction: MutableMap<String, Any?> = HashMap()
-        transaction["user"] = userID
-        transaction["type"] = type_name
-        transaction["amount"] = amount
-        transaction["note"] = note
-        transaction["category"] = category
-        transaction["date"] =
-            SimpleDateFormat("yyyy-MM-dd HH:mm").format(Calendar.getInstance().time)
-
-        db.collection("transactions")
-            .add(transaction)
-            .addOnSuccessListener { documentReference ->
-                Log.d(
-                    this.localClassName,
-                    "DocumentSnapshot added with ID: " + documentReference.id
-                )
-            }
-            .addOnFailureListener { e ->
-                Log.w(
-                    this.localClassName,
-                    "Error adding document",
-                    e
-                )
-            }
-
-
-        if (frequent) {
-
-            val frequentTransaction: MutableMap<String, Any?> = HashMap()
-            frequentTransaction["user"] = userID
-            frequentTransaction["type"] = type_name
-            frequentTransaction["amount"] = amount
-            frequentTransaction["note"] = note
-            frequentTransaction["category"] = category
-
-            db.collection("frequentTransactions")
-                .add(frequentTransaction)
-                .addOnSuccessListener { documentReference ->
-                    Log.d(
-                        this.localClassName,
-                        "DocumentSnapshot added with ID: " + documentReference.id
-                    )
-                }
-                .addOnFailureListener { e ->
-                    Log.w(
-                        this.localClassName,
-                        "Error adding document",
-                        e
-                    )
-                }
-        }
+    private fun selectCategory() {
+        val resultIntent = Intent()
+        resultIntent.putExtra("category", selected)
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
     }
 
     override fun loadIcons(iconList : MutableList<Icon>){
